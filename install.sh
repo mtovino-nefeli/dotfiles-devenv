17176818da1067488809f6398e53e99347402d92
Mike Tovino <mtovino@nefeli.io>
2022-05-04 07:51:34 -0700

    Some niceties

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -ex
 
 cp ssh/authorized_keys ~/.ssh/authorized_keys
+cp profile ~/.profile
+cp gitconfig ~/.gitconfig
+cp gitmessage.tmp ~/.gitmessage.tmp
+cp git-completion.bash ~/.git-completion.bash
